b23dd64650c10ae7e342b33da79deaea62e27cbd
Vimal-106 <159473710+Vimal-106@users.noreply.github.com>
2024-09-22 15:03:42 +0530

    Create Calculator.cpp

---
+++ Calculator.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+// Function to add two numbers by Vimal
+double add(double a, double b) {
+    return a + b;
+}
+
+int main() {
+    double num1, num2;
+
+    cout << "Enter the first number: ";
+    cin >> num1;
+
+    cout << "Enter the second number: ";
+    cin >> num2;
+
+    cout << "Result: " << add(num1, num2) << endl;
+
+    return 0;
+}
